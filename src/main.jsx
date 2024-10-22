cf7becadbd498e99e707d12b40fdaaa14540d421
mrsumityadav <sumitsingh910591@gmail.com>
2024-10-23 00:49:50 +0530

    check eroor

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Context from "./utils/context.jsx";
+import Context from "./utils/context";
 
 createRoot(document.getElementById("root")).render(
   <Context>
